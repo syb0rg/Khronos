709aeb34aaaa6a3de508887f84c79510c5400c3f
syb0rg <darksyb0rg@gmail.com>
2016-07-25 23:59:12 -0500

    Prioritized Libtool dependency build

--- cmake/FindLibtool.cmake
+++ cmake/FindLibtool.cmake
@@ -1,27 +1,24 @@
+# - Try to find Libtool
+# Once done, this will define
 #
-# Check for the presence of LIBTOOL
-#
-# LIBTOOL_FOUND will be true if all components have been found.
+#  LIBTOOL_FOUND - system has sphinxbase
+#  LIBTOOL_INCLUDE_DIRS - the sphinxbase include directories
+#  LIBTOOL_LIBRARIES - link these to use sphinxbase
+
+# Include dir
+find_path(LIBTOOL_INCLUDE_DIR Libtool/config.h)
+
+# Library
+find_library(LIBTOOL_LIBRARY Libtool)
+
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Libtool DEFAULT_MSG LIBTOOL_LIBRARY LIBTOOL_INCLUDE_DIR)
 
-IF (NOT LIBTOOL_FOUND)
-    IF (NOT LIBTOOL_ROOT_DIR)
-        SET(LIBTOOL_ROOT_DIR ${CMAKE_INSTALL_PREFIX})
-    ENDIF ()
-
-    # Determine if the libtool executables exist
-    FOREACH (APPLICATION libtool libtoolize)
-        # Find the executables
-        STRING(TOUPPER ${APPLICATION} APPLICATION_NAME)
-        FIND_PROGRAM(${APPLICATION_NAME}_EXECUTABLE ${APPLICATION} HINTS ${LIBTOOL_ROOT_DIR} ${CMAKE_INSTALL_PREFIX} PATH_SUFFIXES bin)
-    ENDFOREACH()
-
-    # Finalize results
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBTOOL DEFAULT_MSG LIBTOOL_EXECUTABLE)
-    IF (NOT LIBTOOL_FOUND)
-        IF (LIBTOOL_FIND_REQUIRED)
-            MESSAGE(FATAL_ERROR "Could not find LIBTOOL!")
-        ENDIF ()
-    ENDIF ()
-    MARK_AS_ADVANCED(LIBTOOL_ROOT_DIR LIBTOOL_EXECUTABLE LIBTOOLIZE_EXECUTABLE)
+if (LIBTOOL_FOUND)
+    set(LIBTOOL_LIBRARIES ${LIBTOOL_LIBRARY})
+    set(LIBTOOL_INCLUDE_DIRS ${LIBTOOL_INCLUDE_DIR})
+else ()
+    set(LIBTOOL_LIBRARIES)
 endif ()
+
+mark_as_advanced(LIBTOOL_LIBRARY LIBTOOL_LIBRARIES LIBTOOL_INCLUDE_DIR LIBTOOL_INCLUDE_DIRS)
